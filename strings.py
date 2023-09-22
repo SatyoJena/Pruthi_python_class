6dc0918a1f06aac0519896138ba379054438916e
SatyoJena <45891879+SatyoJena@users.noreply.github.com>
2023-09-22 22:51:47 +0530

    escape sequences

--- strings.py
+++ strings.py
@@ -13,6 +13,15 @@ v = s*3
 
 x = 2   +  3   #5
 y = '2' + '3'  #23
-
-
 print(x,y)
+
+# escape sequences
+# \ + some_charcter  = escape sequence
+# but how to get \ itself?
+# add \ to \ 
+print("1. ", "abcd\nefgh") #newline
+print("2. ", "abcd\tefgh") #tab
+print("3. ", "abcd\refgh") #carriage return
+print("4. ", "abcd\fefgh") #line feed
+print("5. ", "abcd\\efgh") #escape the slash itself
+
