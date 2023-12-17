9e0c585271d6fc858669c5def2210ea063d9c05f
SatyoJena <45891879+SatyoJena@users.noreply.github.com>
2023-12-17 21:56:57 +0530

    tuples
    Also gave insight to approaches in C and python regarding printing a pyramid

---
+++ bhanga pyramid.py
@@ -0,0 +1,12 @@
+# the C fatigue way
+n = int(input("Enter N: "))
+for i in range(1,n+1) :
+	for j in range(1,i+1):
+		print("*", end=" ")
+	print()
+
+""" the pyhtonic way
+n = int(input("Enter N: "))
+for i in range(1,n+1):
+	print(" *"*i)
+"""
---
+++ tuples.py
@@ -0,0 +1,8 @@
+l = [(1,2),(2,4),(3,6),(4,8),(5,10)]
+# let l be a table storing x and f(x)
+
+#lets print the table
+print(l)
+print("x  y")
+for x,y in l:
+	print(f"{x}  {y}")
